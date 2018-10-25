efae01d98ed2b8cbb412008f2fb62160c89dc912
Holvoet <kristian_holvoet@vanguard.com>
2018-10-25 11:07:34 -0400

    Added algs4.jar to dependencies

--- src/main/java/com/mournival/Library.java
+++ src/main/java/com/mournival/Library.java
@@ -1,6 +1,10 @@
 package com.mournival;
+
+import edu.princeton.cs.algs4.BinarySearch;
+
 public class Library {
     public boolean someLibraryMethod() {
+    	BinarySearch.indexOf(new int[]{1, 3, 4, 7}, 3);
         return true;
     }
 }
--- src/test/java/com/mournival/LibraryTest.java
+++ src/test/java/com/mournival/LibraryTest.java
@@ -1,11 +1,20 @@
 package com.mournival;
 
 import org.junit.Test;
+
+import edu.princeton.cs.algs4.BinarySearch;
+
 import static org.junit.Assert.*;
 
 public class LibraryTest {
-    @Test public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
-    }
+	@Test
+	public void testSomeLibraryMethod() {
+		Library classUnderTest = new Library();
+		assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+	}
+
+	@Test
+	public void testBinarySearch() {
+		assertTrue(1 == BinarySearch.indexOf(new int[] { 1, 3, 4, 7 }, 3));
+	}
 }
