e6956f10b1f11d65c5fc9a4978865954d568e3a7
Kristian Holvoet <kwh@mournival.com>
2018-11-08 14:33:01 -0500

    Completed Exercise 1.1.31 according to the problem description.

--- src/main/java/com/mournival/algs4/Exercise_01_31.java
+++ src/main/java/com/mournival/algs4/Exercise_01_31.java
@@ -1,44 +1,64 @@
 package com.mournival.algs4;
 
+import java.awt.Color;
+
 import edu.princeton.cs.algs4.StdDraw;
 import edu.princeton.cs.algs4.StdRandom;
 
+/**
+ * Random connections. Write a program that takes as command-line arguments an
+ * integer N and a double value p (between 0 and 1), plots N equally spaced dots
+ * of size .05 on the circumference of a circle, and then, with probability p
+ * for each pair of points, draws a gray line connecting them.
+ *
+ * @author Kristian Holvoet
+ *
+ */
 public class Exercise_01_31 {
 
-    public Exercise_01_31() {
-        super();
-    }
+	private static final double DOT_SIZE = 0.05;
+	private static final Color LINE_COLOR = Color.gray;
+	private static final int CANVAS_SIZE = 1024;
+	private static double LINE_THICKNESS = 0.0002;
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
+		StdDraw.setCanvasSize(CANVAS_SIZE, CANVAS_SIZE);
+		
+		double center = CANVAS_SIZE / 2; // Circle Center is (c,c)
+		StdDraw.setScale(0, 2 * (1 + center));
+		// StdDraw.circle(c, c, r);
+		
+		int n = Integer.parseInt(args[0]);
+		double alpha = 2.0 * Math.PI / n;
+		int r = CANVAS_SIZE / 2 - 2;
+		plotPoints(n, center, r, alpha);
+		
+		double p = Double.parseDouble(args[1]);
+		plotLines(n, center, r, alpha, p);
+	}
 
-        //        int n = Integer.parseInt(args[0]);
-        //        double p = Double.parseDouble(args[1]);
-        int n = 64;
-        double p = 0.50;
+	private static void plotLines(int n, double center, int r, double alpha, double p) {
+		StdDraw.setPenColor(LINE_COLOR);
+		StdDraw.setPenRadius(LINE_THICKNESS);
+		StdDraw.setPenColor(LINE_COLOR);
 
-        int dim = 640;
-        StdDraw.setCanvasSize(dim, dim);
+		for (int i = 0; i < n; i++) {
+			for (int j = i + 1; j < n; ++j) {
+				if (StdRandom.uniform() < p) {
+					double theta_0 = alpha * i;
+					double theta_1 = alpha * j;
+					StdDraw.line(center + r * Math.cos(theta_0), center + r * Math.sin(theta_0),
+							center + r * Math.cos(theta_1), center + r * Math.sin(theta_1));
+				}
+			}
+		}
+	}
 
-        double c = dim / 2; // Center is (c,c)
-
-        StdDraw.setScale(0, 2 * (1 + c));
-//        StdDraw.circle(c, c, r);
-        StdDraw.setPenRadius(0.005);
-
-        int r = dim * 49 / 100;
-        double alpha = 2.0 * Math.PI / n;
-        for (int i = 0; i < n; ++i) {
-            double theta = alpha * i;
-            StdDraw.point(c + r * Math.cos(theta), c + r * Math.sin(theta));
-        }
-
-        StdDraw.setPenRadius(0.002);
-        for (int i = 0; i < n; i++) {
-            if (StdRandom.uniform() < p) {
-                double theta_0 = alpha * i;
-                double theta_1 = alpha * (i + 1);
-                StdDraw.line(c + r * Math.cos(theta_0), c + r * Math.sin(theta_0), c + r * Math.cos(theta_1), c + r * Math.sin(theta_1));
-            }
-        }
-    }
+	private static void plotPoints(int n, double center, int r, double alpha) {
+		StdDraw.setPenRadius(DOT_SIZE);
+		for (int i = 0; i < n; ++i) {
+			double theta = alpha * i;
+			StdDraw.point(center + r * Math.cos(theta), center + r * Math.sin(theta));
+		}
+	}
 }
